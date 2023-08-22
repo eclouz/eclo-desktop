a1e682c1ffc8a8dbdd2658ae5ffb2bacee7d4b7c
HasanboyXomidov <hasanboyxomidov04@gmail.com>
2023-08-22 23:30:57 +0500

    Brand : Concrete : GetAllBrands Finished beckEnd

--- src/Integrated/ServiceLayer/API.cs
+++ src/Integrated/ServiceLayer/API.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Win32;
+﻿using Dtos.Auth;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,17 @@ namespace Integrated.ServiceLayer;
 
 public class API
 {
+    //Base Url
     public static readonly string BASE_URL = "http://eclo.uz:8080/api/";
+    
+    //Login Register Confirm Password URLs
     public static readonly string CREATE_USER = BASE_URL + "auth/register";
     public static readonly string SEND_CODE_REGISTER = BASE_URL + "auth/register/send-code";
     public static readonly string VERIFY_REGISTER = BASE_URL + "auth/register/verify";
     public static readonly string LOGIN = BASE_URL + "auth/login";
+    //Login Register Confirm Password URLs
+
+    //Brand Urls
+    public static readonly string COMMON_BRANDS = BASE_URL + "common/brands";
+    //Brand Urls
 }
---
+++ src/Integrated/ServiceLayer/Brand/Concrete/BrandService.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModels.Brands;
+
+namespace Integrated.ServiceLayer.Brand.Concrete;
+
+public class BrandService : IBrandService
+{
+    public async Task<IList<MensBrandsViewModels>> GetAllBrands(int page=1)
+    {
+        using (var client = new HttpClient())
+        {            
+            var request = new HttpRequestMessage(HttpMethod.Get, API.COMMON_BRANDS+
+                $"?page={page}");
+            var content = new StringContent("", null, "text/plain");
+            request.Content = content;
+
+            var res = await client.GetAsync(client.BaseAddress);
+
+            string rsp = await res.Content.ReadAsStringAsync();
+
+            var response = await client.SendAsync(request);
+
+            IEnumerable<MensBrandsViewModels> readBrands = JsonConvert.DeserializeObject<IEnumerable<MensBrandsViewModels>>(rsp);
+
+            List<MensBrandsViewModels> mensBrandsViewModels = new List<MensBrandsViewModels>();
+
+            foreach(var i in readBrands)
+            {
+                mensBrandsViewModels.Add(new MensBrandsViewModels()
+                {
+                    Name = i.Name,
+                    BrandIconPath = i.BrandIconPath,
+                    CreatedAt = i.CreatedAt,
+                    UpdatedAt = i.UpdatedAt,
+                    Id = i.Id
+                });
+            }
+            return mensBrandsViewModels;
+        }
+    }
+}
