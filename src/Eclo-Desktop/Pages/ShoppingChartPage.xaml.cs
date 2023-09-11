9954e0388205458ae9620ec24efe3812af97459e
“javohirtohirovich” <124333197+javohirtohirovich@users.noreply.github.com>
2023-09-11 13:26:54 +0500

    Shopping Chart Page

---
+++ src/Eclo-Desktop/Pages/ShoppingChartPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Eclo_Desktop.Components.ShoppingCharts;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Eclo_Desktop.Pages
+{
+    /// <summary>
+    /// Interaction logic for ShoppingChartPage.xaml
+    /// </summary>
+    public partial class ShoppingChartPage : Page
+    {
+        public ShoppingChartPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+
+            
+        }
+
+        private void StackPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == System.Windows.Input.MouseButton.Left)
+            {
+                
+            }
+        }
+    }
+}
