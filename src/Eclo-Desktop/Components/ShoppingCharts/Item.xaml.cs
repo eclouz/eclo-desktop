bc43a8edbda3604efb545020389ccf246ae0fd80
“javohirtohirovich” <124333197+javohirtohirovich@users.noreply.github.com>
2023-09-12 16:33:23 +0500

    Create ShoppingChart Component

--- src/Eclo-Desktop/Components/ShoppingCharts/Item.xaml.cs
+++ src/Eclo-Desktop/Components/ShoppingCharts/Item.xaml.cs
@@ -24,57 +24,12 @@ namespace Eclo_Desktop.Components.ShoppingCharts
         {
             InitializeComponent();
         }
-        public ImageSource Source
+
+
+        private void btProductDelete(object sender, RoutedEventArgs e)
         {
-            get { return (ImageSource)GetValue(SourceProperty); }
-            set { SetValue(SourceProperty, value); }
+
         }
-
-        public static readonly DependencyProperty SourceProperty = DependencyProperty.Register("Source", typeof(ImageSource), typeof(Item));
-
-
-        public string Title
-        {
-            get { return (string)GetValue(TitleProperty); }
-            set { SetValue(TitleProperty, value); }
-        }
-
-        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(Item));
-
-
-        public string Ref
-        {
-            get { return (string)GetValue(RefProperty); }
-            set { SetValue(RefProperty, value); }
-        }
-
-        public static readonly DependencyProperty RefProperty = DependencyProperty.Register("Ref", typeof(string), typeof(Item));
-
-
-        public string Color
-        {
-            get { return (string)GetValue(ColorProperty); }
-            set { SetValue(ColorProperty, value); }
-        }
-
-        public static readonly DependencyProperty ColorProperty = DependencyProperty.Register("Color", typeof(string), typeof(Item));
-
-
-        public string Count
-        {
-            get { return (string)GetValue(CountProperty); }
-            set { SetValue(CountProperty, value); }
-        }
-
-        public static readonly DependencyProperty CountProperty = DependencyProperty.Register("Count", typeof(string), typeof(Item));
-
-
-        public string Price
-        {
-            get { return (string)GetValue(PriceProperty); }
-            set { SetValue(PriceProperty, value); }
-        }
-
-        public static readonly DependencyProperty PriceProperty = DependencyProperty.Register("Price", typeof(string), typeof(Item));
     }
+
 }
